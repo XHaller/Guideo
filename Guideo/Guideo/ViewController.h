f499822494de0a750dc60554fd87316971914653
Xiaochen <weixc123@gmail.com>
2015-04-04 22:10:48 -0400

    Init the projects and readme

---
+++ Guideo/Guideo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Guideo
+//
+//  Created by wei on 15/4/4.
+//  Copyright (c) 2015年 Guideo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Guideo/Guideo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Guideo
+//
+//  Created by wei on 15/4/4.
+//  Copyright (c) 2015年 Guideo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
