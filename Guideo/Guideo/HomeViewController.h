a210c343c5a6a5b83de2327dae38670e88cf09e8
Jihan Li <jh9228.li@gmail.com>
2015-04-11 14:51:48 -0400

    Integeration

---
+++ Guideo/Guideo/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  Guideo
+//
+//  Created by 亮亮 李 on 15/4/11.
+//  Copyright (c) 2015年 Guideo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UITabBarController
+
+@end
--- Guideo/Guideo/ViewController.h
+++ Guideo/Guideo/ViewController.h
@@ -1,15 +1,29 @@
 //
 //  ViewController.h
-//  Guideo
+//  Login
 //
-//  Created by wei on 15/4/11.
-//  Copyright (c) 2015年 Guideo. All rights reserved.
+//  Created by 亮亮 李 on 15/4/5.
+//  Copyright (c) 2015年 Kyran. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UITextFieldDelegate>
 
+@property (nonatomic, weak) IBOutlet UILabel *greetlabel1;
+@property (nonatomic, weak) IBOutlet UILabel *greetlabel2;
+@property (weak, nonatomic) IBOutlet UILabel *usernameText;
+@property (nonatomic, weak) IBOutlet UILabel *passwordText;
+@property (nonatomic, weak) IBOutlet UITextField *usernameField;
+@property (nonatomic, weak) IBOutlet UITextField *passwordField;
+@property (weak, nonatomic) IBOutlet UIButton *signinButton;
+@property (nonatomic, weak) IBOutlet UIButton *signupButton;
+
+- (IBAction)signinDown:(id)sender;
+- (IBAction)signinClicked:(id)sender;
+- (IBAction)tapReturn:(id)sender;
+- (BOOL) textFieldShouldReturn:(UITextField *)textField;
+- (void) alertStatus:(NSString *)msg :(NSString *)title :(int) tag;
 
 @end
 
