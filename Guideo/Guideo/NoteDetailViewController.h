ef8afedeceb05813c95ab7b1b61ce2d412b12823
Xiaochen <weixc123@gmail.com>
2015-05-05 18:26:49 -0400

    add notedetailviewcontroller

---
+++ Guideo/Guideo/NoteDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  NoteDetailViewController.h
+//  Guideo
+//
+//  Created by wei on 15/5/5.
+//  Copyright (c) 2015年 Guideo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NoteDetailViewController : UIViewController
+
+@end
---
+++ Guideo/Guideo/NoteDetailViewController.m
@@ -0,0 +1,19 @@
+//
+//  NoteDetailViewController.m
+//  Guideo
+//
+//  Created by wei on 15/5/5.
+//  Copyright (c) 2015年 Guideo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "NoteDetailViewController.h"
+
+@interface NoteDetailViewController ()
+
+@end
+
+@implementation NoteDetailViewController
+
+
+@end
