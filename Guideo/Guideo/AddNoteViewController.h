e92c3bb6a7fc5217359a66f7a0db22d63eb11082
Xiaochen <weixc123@gmail.com>
2015-05-12 20:57:09 -0400

    add new notes

---
+++ Guideo/Guideo/AddNoteViewController.h
@@ -0,0 +1,23 @@
+//
+//  AddNoteViewController.h
+//  Guideo
+//
+//  Created by wei on 15/5/12.
+//  Copyright (c) 2015年 Guideo. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+#import "DoImagePickerController.h"
+
+@interface AddNoteViewController : UIViewController <DoImagePickerControllerDelegate>
+
+@property (weak, nonatomic) IBOutlet UIImageView    *iv1;
+@property (strong, nonatomic)   NSArray             *aIVs;
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl     *sgColumnCount;
+@property (weak, nonatomic) IBOutlet UISegmentedControl     *sgMaxCount;
+
+- (IBAction)onShowImagePicker:(id)sender;
+
+@end
