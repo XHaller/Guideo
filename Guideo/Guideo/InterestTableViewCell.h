001cc160cbcac4a72f68a97c82438f67a7484892
Jihan Li <jh9228.li@gmail.com>
2015-05-17 14:27:14 -0400

    Interest Selection

---
+++ Guideo/Guideo/InterestTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  InterestTableViewCell.h
+//  Guideo
+//
+//  Created by 亮亮 李 on 15/5/17.
+//  Copyright (c) 2015年 Guideo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InterestTableViewCell : UITableViewCell
+
+@end
