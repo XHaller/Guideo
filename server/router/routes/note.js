2e06b3bdb78cc5cf0c39f1aa4a506354c0e9ae5f
Mahd <mahdtauseef@gmail.com>
2015-05-22 08:39:41 -0400

    fixing note

--- server/router/routes/note.js
+++ server/router/routes/note.js
@@ -19,14 +19,31 @@ router.post('/', function(req, res){
 		var id=req.body.id;
 		console.log("The note id is: "+ id);
 
-		var queryString = 'SELECT * FROM Notes WHERE Notes.id="' + id +'";';
-		console.log("Querying db for notes with id " + id);
+		var queryString = 'SELECT * FROM Notes;';
 		console.log("This is the query " + queryString);
 		connection.query(queryString, function(err, rows, fields) {
 		    if (err) console.log(err);
-			
+			var a = "[";
 			if (rows.length > 0) {
-				res.end(JSON.stringify({ note: 1, id: rows[0].nid, title: rows[0].title}));
+				for (var i = 0; i < rows.length; i++) {
+					var b = "";
+					var uid = rows[i].uid;
+					b = b + "{ \"topic\": \""+rows[i].title + "\", \"user\": \"";
+					var qString = 'SELECT name FROM Users WHERE Users.uid ="' + uid + '";';
+					connection.query(qString, function(err, rows, fields){
+						var c = b + rows[0].name + "\"}";
+						console.log(c);
+						a = a + c;
+						if (i==rows.length-1) {
+							a.slice(0,-1);
+							a = a+"]";
+							console.log("Returning:" +a);
+							res.end(JSON.parse(a));
+						}
+					});
+				}
+
+				//res.end(JSON.stringify({ note: 1, id: rows[0].nid, title: rows[0].title}));
 			} else {
 				    res.end(JSON.stringify({ note: 1 }));
 			
